4a3ae8e0ad0c94e3583dfb2f057094c3ae28fc7a
YJ04-DEV <kr_dev@icloud.com>
2023-03-25 03:51:32 +0900

    입장 시 타이틀 출력

--- src/main/java/org/extension/services/JoinEventService.java
+++ src/main/java/org/extension/services/JoinEventService.java
@@ -1,9 +1,16 @@
 package org.extension.services;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinEventService {
     public void setJoinMessage(PlayerJoinEvent playerJoinEvent) {
         playerJoinEvent.setJoinMessage("[ + ] " + playerJoinEvent.getPlayer().getName());
+        sendTitle(playerJoinEvent);
+    }
+
+    public void sendTitle(PlayerJoinEvent playerJoinEvent) {
+        Player player = playerJoinEvent.getPlayer();
+        player.sendTitle("title", "subtitle", 60, 60, 20);
     }
 }
