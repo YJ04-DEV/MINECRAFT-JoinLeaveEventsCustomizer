0ede76eaa72d44212fadfed1a05da5b55809fdb3
YJ04-DEV <kr_dev@icloud.com>
2023-03-25 23:23:34 +0900

    config 기본값 구성

--- src/main/java/org/extension/config/ConfigYml.java
+++ src/main/java/org/extension/config/ConfigYml.java
@@ -1,10 +1,29 @@
 package org.extension.config;
 
+import org.extension.Main;
 import org.extension.utils.ConfigGenerator;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConfigYml {
     private final ConfigGenerator configGenerator = new ConfigGenerator();
     public void setup() {
         configGenerator.generator();
+        getConfigValue();
+    }
+
+    public void getConfigValue() {
+        Map<String, Map<String, String>> events = new HashMap<>();
+        events.put("JoinEvent", new HashMap<>());
+        events.get("JoinEvent").put("message", Main.getInstance().getConfig().getString("JoinEvent.message"));
+        events.get("JoinEvent").put("messageColor", Main.getInstance().getConfig().getString("JoinEvent.messageColor"));
+        events.get("JoinEvent").put("playerColor", Main.getInstance().getConfig().getString("JoinEvent.playerColor"));
+
+        events.put("QuitEvent", new HashMap<>());
+        events.get("QuitEvent").put("message", Main.getInstance().getConfig().getString("QuitEvent.message"));
+        events.get("QuitEvent").put("messageColor", Main.getInstance().getConfig().getString("QuitEvent.messageColor"));
+        events.get("QuitEvent").put("playerColor", Main.getInstance().getConfig().getString("QuitEvent.playerColor"));
+        System.out.println(events.get("JoinEvent").get("message"));
     }
 }
--- src/main/java/org/extension/utils/ConfigGenerator.java
+++ src/main/java/org/extension/utils/ConfigGenerator.java
@@ -3,14 +3,27 @@ package org.extension.utils;
 import org.extension.Main;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class ConfigGenerator {
     public void generator() {
         File configFile = new File(Main.getInstance().getDataFolder(), "config.yml");
-        if (configFile.exists()) {
-
-        } else {
-            Main.getInstance().getConfig().set("test", "test");
+        if (!configFile.exists()) {
+            Main.getInstance().getConfig().options().copyDefaults(true);
+            String[] events = {"JoinEvent", "QuitEvent"};
+            List<String> keys = Arrays.asList("message", "messageColor", "playerColor");
+            for (String event : events) {
+                for (String key : keys) {
+                    if (key.equals("message")) {
+                        Main.getInstance().getConfig().addDefault(event + "." + key, switch (event) {
+                            case "JoinEvent" -> "#PLAYER# 님이 접속하셨습니다.";
+                            case "QuitEvent" -> "#PLAYER# 님이 퇴장하셨습니다.";
+                            default -> "#FFFFFF";
+                        });
+                    } else Main.getInstance().getConfig().addDefault(event + "." + key, "#FFFFFF");
+                }
+            }
             Main.getInstance().saveConfig();
         }
     }
